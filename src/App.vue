a0df2f28dcf011b44f6336c3426e21451fc26ac1
Alix <alix@spacefoot.com>
2020-05-09 17:17:34 +0200

    menu updated

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,19 @@
 <template>
   <v-app>
     <v-navigation-drawer v-model="drawer" app>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="title">
+            OpenFlight
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            Payez au meilleur moment !
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+  
+      <v-divider></v-divider>
+
       <v-list dense>
         <v-list-item link>
           <v-list-item-action>
